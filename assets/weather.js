feeec98f674dd81e88fbad54a57acd9112e34ce2
Corey Post <corey.post0120@gmail.com>
2020-07-02 22:10:29 -0600

    fixed javascript

--- assets/weather.js
+++ assets/weather.js
@@ -25,30 +25,6 @@ $(document).ready(function(){
         showWeather(false)
     });
 
-    //function for search history for cities
-    function showStorage(){
-        $(".list-group").empty();
-        //get search city history from locale storage
-        cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
-
-        if(!cityHistory){
-            //If there is no history start with a default city
-            cityHistory = ["Salt lake city"];
-            localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
-        }
-
-        //create loop to make list of cities previously searched from local storage
-        location = cityHistory[cityHistory.length-1];
-        showWeather(false);
-
-        //create for loop to take info from locale storage and display on the list
-        for(i=0; i<cityHistory.length; i++){
-            var newLi = $("<li>").text(cityHistory[i]);
-            newLi.addClass("list-group-item");
-            $(".list-group").append(newLi);
-        }
-    }
-
     //function to prevent buttons replicating with the new information
     function showWeather(saveHistory){
         if(location !== "" && saveHistory){
@@ -164,5 +140,29 @@ $(document).ready(function(){
                 }
             });
         });
+    }
+
+    //function for search history for cities
+    function showStorage(){
+        $(".list-group").empty();
+        //get search city history from locale storage
+        cityHistory = JSON.parse(localStorage.getItem("cityHistory"));
+
+        if(!cityHistory){
+            //If there is no history start with a default city
+            cityHistory = ["Salt lake city"];
+            localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+        }
+
+        //create loop to make list of cities previously searched from local storage
+        location = cityHistory[cityHistory.length-1];
+        showWeather(false);
+
+        //create for loop to take info from locale storage and display on the list
+        for(i=0; i<cityHistory.length; i++){
+            var newLi = $("<li>").text(cityHistory[i]);
+            newLi.addClass("list-group-item");
+            $(".list-group").append(newLi);
+        }
     }
 });
